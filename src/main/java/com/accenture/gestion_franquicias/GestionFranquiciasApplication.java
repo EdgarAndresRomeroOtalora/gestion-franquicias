d5adf64c90ef8b9cfd10b05dd0a48dfbc82f00a0
Edgar Andres Romero Otalora <andres.romero.otalora@gmail.com>
2024-10-07 16:44:53 -0500

    Versión inicial aplicación en Spring Boot

---
+++ src/main/java/com/accenture/gestion_franquicias/GestionFranquiciasApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.gestion_franquicias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFranquiciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFranquiciasApplication.class, args);
+	}
+
+}
