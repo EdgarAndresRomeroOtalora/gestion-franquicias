0dec1b5a220dc6bcec031db50818e78d7b7db6d8
Edgar Andres Romero Otalora <andres.romero.otalora@gmail.com>
2024-10-08 09:57:26 -0500

    Endpoint para agregar un producto y para agregar una sucursal

--- src/main/java/com/accenture/gestion_franquicias/controllers/FranquiciaController.java
+++ src/main/java/com/accenture/gestion_franquicias/controllers/FranquiciaController.java
@@ -1,15 +1,13 @@
 package com.accenture.gestion_franquicias.controllers;
 
 import com.accenture.gestion_franquicias.entities.Franquicia;
+import com.accenture.gestion_franquicias.entities.Producto;
 import com.accenture.gestion_franquicias.entities.Sucursal;
 import com.accenture.gestion_franquicias.services.FranquiciaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,5 +25,20 @@ public class FranquiciaController {
         franquicia.setSucursales(sucursales);
         Franquicia franquicia1 = franquiciaService.crearFranquicia(franquicia);
         return new ResponseEntity<>(franquicia1, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{franquiciaId}/sucursales")
+    public ResponseEntity<Franquicia> agregarSucursal(@PathVariable Long franquiciaId, @RequestBody Sucursal sucursal) {
+        System.out.println("Ingreso al metodo agregarSucursal");
+        List<Producto> productos = new ArrayList<>();
+        sucursal.setProductos(productos);
+        Franquicia franquicia = franquiciaService.agregarSucursal(franquiciaId, sucursal);
+        return new ResponseEntity<>(franquicia, HttpStatus.OK);
+    }
+
+    @PostMapping("/{franquiciaId}/sucursales/{sucursalId}/productos")
+    public ResponseEntity<Sucursal> agregarProducto(@PathVariable Long franquiciaId, @PathVariable Long sucursalId, @RequestBody Producto producto) {
+        Sucursal sucursal = franquiciaService.agregarProducto(franquiciaId, sucursalId, producto);
+        return new ResponseEntity<>(sucursal, HttpStatus.OK);
     }
 }
--- src/main/java/com/accenture/gestion_franquicias/services/FranquiciaService.java
+++ src/main/java/com/accenture/gestion_franquicias/services/FranquiciaService.java
@@ -1,9 +1,13 @@
 package com.accenture.gestion_franquicias.services;
 
 import com.accenture.gestion_franquicias.entities.Franquicia;
+import com.accenture.gestion_franquicias.entities.Producto;
+import com.accenture.gestion_franquicias.entities.Sucursal;
 import com.accenture.gestion_franquicias.repositories.FranquiciaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class FranquiciaService {
@@ -13,5 +17,41 @@ public class FranquiciaService {
 
     public Franquicia crearFranquicia(Franquicia franquicia) {
         return franquiciaRepository.save(franquicia);
+    }
+
+    public Franquicia agregarSucursal(Long franquiciaId, Sucursal sucursal) {
+        System.out.println("Ingreso al metodo agregarSucursal");
+        Optional<Franquicia> franquiciaOptional = franquiciaRepository.findById(franquiciaId);
+
+        if (franquiciaOptional.isPresent()) {
+            Franquicia franquicia = franquiciaOptional.get();
+            franquicia.getSucursales().add(sucursal);
+            return franquiciaRepository.save(franquicia);
+        }else{
+            throw new RuntimeException("No se encontro la franquicia");
+        }
+    }
+
+    public Sucursal agregarProducto(Long franquiciaId, Long sucursalId, Producto producto){
+        Optional<Franquicia> franquiciaOptional = franquiciaRepository.findById(franquiciaId);
+        if (franquiciaOptional.isPresent()) {
+            Franquicia franquicia = franquiciaOptional.get();
+            Optional<Sucursal> sucursalOptional = franquicia.getSucursales()
+                    .stream()
+                    .filter(sucursal -> sucursal.getId().equals(sucursalId))
+                    .findFirst();
+            if(sucursalOptional.isPresent()){
+                Sucursal sucursal = sucursalOptional.get();
+                sucursal.getProductos().add(producto);
+                return franquiciaRepository.save(franquicia).getSucursales().stream()
+                        .filter(suc -> suc.getId().equals(sucursalId))
+                        .findFirst()
+                        .orElseThrow(() -> new RuntimeException("No se encontro la sucursal"));
+            }else{
+                throw new RuntimeException("No se encontro la sucursal");
+            }
+        }else{
+            throw new RuntimeException("No se encontro la franquicia");
+        }
     }
 }
