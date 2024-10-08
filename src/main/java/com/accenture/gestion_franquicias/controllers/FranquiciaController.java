5a68b84b5588d71ac7d0200735b473d174bbc710
Edgar Andres Romero Otalora <andres.romero.otalora@gmail.com>
2024-10-08 09:08:22 -0500

    Ajuste en la creación de la franquicia

--- src/main/java/com/accenture/gestion_franquicias/controllers/FranquiciaController.java
+++ src/main/java/com/accenture/gestion_franquicias/controllers/FranquiciaController.java
@@ -1,6 +1,7 @@
 package com.accenture.gestion_franquicias.controllers;
 
 import com.accenture.gestion_franquicias.entities.Franquicia;
+import com.accenture.gestion_franquicias.entities.Sucursal;
 import com.accenture.gestion_franquicias.services.FranquiciaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/franquicias")
@@ -19,6 +23,8 @@ public class FranquiciaController {
 
     @PostMapping
     public ResponseEntity<Franquicia> crearFranquicia(@RequestBody Franquicia franquicia) {
+        List<Sucursal> sucursales = new ArrayList<>();
+        franquicia.setSucursales(sucursales);
         Franquicia franquicia1 = franquiciaService.crearFranquicia(franquicia);
         return new ResponseEntity<>(franquicia1, HttpStatus.CREATED);
     }
