555a4860ea19a2440bc6dfb5b51728f94671b880
Edgar Andres Romero Otalora <andres.romero.otalora@gmail.com>
2024-10-07 22:12:17 -0500

    Conexion con DB MySQL, y se creo la tabla producto usando hibernate

---
+++ src/main/java/com/accenture/gestion_franquicias/entities/Producto.java
@@ -0,0 +1,24 @@
+package com.accenture.gestion_franquicias.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "producto")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private int cantidad;
+}
